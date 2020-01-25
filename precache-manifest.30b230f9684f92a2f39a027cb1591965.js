1cfcfd777ae28f9c3b0bdfa9871c33cbb06eb2e5
dbuzzin <d.pearson1991@googlemail.com>
2020-01-25 23:33:35 +0000

    Updates

---
+++ precache-manifest.30b230f9684f92a2f39a027cb1591965.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04a51d0589d894e7c49aaf3d4d383fb0",
+    "url": "/task-manager-app/index.html"
+  },
+  {
+    "revision": "d77ad2c40c1c69c31428",
+    "url": "/task-manager-app/static/css/main.4c639846.chunk.css"
+  },
+  {
+    "revision": "56579739dd8a5dc6ad04",
+    "url": "/task-manager-app/static/js/2.a57e28a9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/task-manager-app/static/js/2.a57e28a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d77ad2c40c1c69c31428",
+    "url": "/task-manager-app/static/js/main.02727cc8.chunk.js"
+  },
+  {
+    "revision": "6477df8616242563770d",
+    "url": "/task-manager-app/static/js/runtime-main.84ae6c9a.js"
+  }
+]);
